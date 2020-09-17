d23905387a3e65ea4394b4f1edbfe311a3a0a014
aurelio.mex <aurelio.mf1315@gmail.com>
2020-09-17 11:22:30 -0500

    fixing other bugs redirect and distinct rows

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,20 +5,19 @@ class RecipesController < ApplicationController
   end
 
   def search
-    recipe_filter = params[:cuisine].present? ? "recipes.cuisione like '#{params[:cuisine]}'" : ""
-    ingredient = params[:ingredient].present? ? "ingredients.name like '#{params[:ingredient]}'" : ""
+    recipe_filter = params[:cuisine].present? ? "recipes.cuisine like '%#{params[:cuisine]}%'" : ""
+    ingredient = params[:ingredient].present? ? "ingredients.name like '%#{params[:ingredient]}%'" : ""
     ##
     @result = []
     if recipe_filter.present? or ingredient.present?
-      @result =   Recipe..joins(:ingredient).where( receip_filter + ingredient )
+      @result =   Recipe.joins(:ingredients).where( recipe_filter + ingredient)
+                      .distinct
     else
       @result = Recipe.all
     end
     ##
 
-    respond_to do |format|
-       format.html {redirect_to result_recipes_path(@result)}
-    end
+    render :result, assigns: {result: @result}
 
 
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,23 @@
 puts "In order to seed the database, run:"
-puts "$ bin/rake db:fixtures:load"
-puts "Your database will be filled with test fixtures which are quite good"
-puts "for development, too."
+#puts "$ bin/rake db:fixtures:load"
+#puts "Your database will be filled with test fixtures which are quite good"
+#puts "for development, too."
 
-recipe_one = Recipe.create(name: "recipe onions", instructions: "cortar las cebollas", cooking_time: 10)
+recipe_one = Recipe.create(cuisine: "mexican", name: "recipe onions", instructions: "cortar las cebollas", cooking_time: 10)
 Ingredient.create(name: "onion", recipe_id: recipe_one.id)
 
-recipe_two = Recipe.create(name: "ribs", instructions: "put the rib on the grill", cooking_time: 5)
+recipe_two = Recipe.create(cuisine: "mexican", name: "ribs", instructions: "put the rib on the grill", cooking_time: 5)
 Ingredient.create(name: "ribs", recipe_id: recipe_two.id)
 
-recipe_three = Recipe.create(name: "tomato sauce", instructions: "roast the tomatoes and blend with chili and coriander", cooking_time: 8)
+recipe_three = Recipe.create(cuisine: "mexican", name: "tomato sauce", instructions: "roast the tomatoes and blend with chili and coriander", cooking_time: 8)
 Ingredient.create(name: "tomato", recipe_id: recipe_three.id)
 Ingredient.create(name: "chili", recipe_id: recipe_three.id)
 Ingredient.create(name: "coriander", recipe_id: recipe_three.id)
+
+recipe_four = Recipe.create(cuisine: "italian", name: "cheese pizza", instructions: "add lots of cheese to pizza bread and microwave", cooking_time: 25)
+Ingredient.create(name: "tomato", recipe_id: recipe_four.id)
+Ingredient.create(name: "bread", recipe_id: recipe_four.id)
+Ingredient.create(name: "cheese", recipe_id: recipe_four.id)
+
 
 puts "END."
